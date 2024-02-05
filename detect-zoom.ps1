444e288eb0b5ef9d6b783c2f17c28ab3111cd4e4
mibmamun <87307414+mibmamun@users.noreply.github.com>
2024-02-05 16:38:04 +0100

    Add files via upload

---
+++ detect-zoom.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = "SilentlyContinue"
+
+$AppName = "*Zoom (32-bit)*"
+$AppVersion = "5.17.31859" 
+
+
+$Apps = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\) | Get-ItemProperty | select DisplayName, DisplayVersion
+$Apps += (Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\) | Get-ItemProperty | select DisplayName, DisplayVersion
+
+
+$AppFound = $Apps | Where-Object {
+($_.DisplayName -like $AppName) -and ([version]$_.DisplayVersion -ge [version]$AppVersion)
+}
+
+if ($AppFound) 
+{Write-Output "Installed"}
